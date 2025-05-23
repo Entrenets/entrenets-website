3c82daf542fbb2e4d0736c0e2204d1f7aba8f392
Alba-Rodriguez <alba.rodriguez@entrenets.com>
2025-05-23 12:59:47 +0200

    Add contact form (#8)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
   "use strict";
   // Scroll to top
-  $("a[href='#top']").click(function() {
+  $("a[href='#top']").click(function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
   });
@@ -10,16 +10,16 @@ $(document).ready(function() {
   $('a.scroll-to').on('click', function (event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
     }, 700);
     event.preventDefault();
   });
 
-  $('.site-testimonial-item').on('mouseenter', function(){
+  $('.site-testimonial-item').on('mouseenter', function () {
     $('.site-testimonial-item').addClass('inactive');
     $(this).removeClass('inactive').addClass('active');
   });
-  $('.site-testimonial-item').on('mouseleave', function(){
+  $('.site-testimonial-item').on('mouseleave', function () {
     $('.site-testimonial-item').removeClass('inactive');
     $('.site-testimonial-item').removeClass('active');
   });
@@ -34,3 +34,28 @@ $(window).on('scroll', function () {
     $('.site-navigation').removeClass('nav-bg');
   }
 });
+
+// Blocks certain e-mail domains
+document.getElementById("email").addEventListener("input", function () {
+  const blockedDomains = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "gmx.de"];
+  const emailInput = this.value;
+  const emailDomain = emailInput.split("@")[1];
+
+  if (emailDomain && blockedDomains.includes(emailDomain.toLowerCase())) {
+    this.setCustomValidity("Please enter a company email.");
+  } else {
+    this.setCustomValidity("");
+  }
+});
+
+//Adds http or https if user does not. 
+document.getElementById("company-website").addEventListener("blur", function () {
+  if (this.value && !this.value.startsWith("http://") && !this.value.startsWith("https://")) {
+    this.value = "https://" + this.value;
+  }
+});
+
+
+
+
+
--- src/contact.md
+++ src/contact.md
@@ -1,8 +1,13 @@
 ---
 layout: "layouts/contact.njk"
-title: "Don’t be shy. Say "
-subTitle: Hello.
+stylesheet: "contact.module.css"
+
+image: /assets/images/contact/email.png
 formTitle: "Tell us about your project"
 btnText: "Send Message"
+title: "Let's talk!"
+subTitle: We’d love to hear from you! Fill out the form below and we’ll get back to you within 24–48 business hours.
+requiredFieldIndicator: true
+requiredFieldStyle: "color: #EB3678; margin-left: 4px;"
 ---
 
